13d0172629d2b397d9ca8eab79d9c84b979dfed2
ywwywwyww <weeerrr720@qq.com>
2021-07-16 09:08:43 +0800

    Add B-Tree test

---
+++ other projects/b_tree_test/bt_main.cc
@@ -0,0 +1,36 @@
+//
+// Created by yww on 2021/7/16.
+//
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include "../binary search trees/b_tree.h"
+
+int a[10000010];
+
+int main() {
+  const int m = 29;
+  std::cout << sizeof(BTreeNode<int, m>) << std::endl;
+  srand(0);
+  int n = 10000000;
+  for (int i = 1; i <= n; i++) {
+    a[i] = rand();
+  }
+  BTree<int, m> bt_set;
+  for (int i = 1; i <= n; i++) {
+    bt_set.Insert(a[i]);
+  }
+
+  std::cout << "press enter key to continue" << std::endl;
+  std::cin.get();
+
+  int sum = 0;
+  for (int i = 1; i <= n; i++) {
+    if (bt_set.Find(a[i])) {
+      sum ^= a[i];
+    }
+  }
+  std::cout << "sum = " << sum << std::endl;
+  return 0;
+}
---
+++ other projects/b_tree_test/rbt_main.cc
@@ -0,0 +1,34 @@
+//
+// Created by yww on 2021/7/16.
+//
+
+#include <cstdio>
+#include <set>
+#include <cstdlib>
+#include <iostream>
+
+int a[10000010];
+
+int main() {
+  srand(0);
+  int n = 10000000;
+  for (int i = 1; i <= n; i++) {
+    a[i] = rand();
+  }
+  std::set<int> rbt_set;
+  for (int i = 1; i <= n; i++) {
+    rbt_set.insert(a[i]);
+  }
+
+  std::cout << "press enter key to continue" << std::endl;
+  std::cin.get();
+
+  int sum = 0;
+  for (int i = 1; i <= n; i++) {
+    if (rbt_set.count(a[i])) {
+      sum ^= a[i];
+    }
+  }
+  std::cout << "sum = " << sum << std::endl;
+  return 0;
+}
