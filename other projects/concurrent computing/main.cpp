47b3eb08d39a150c15d91c470b9de8039889ec02
yww <weeerrr720@qq.com>
2021-07-18 20:07:22 +0800

    Update concurrent computing

--- other projects/concurrent computing/main.cpp
+++ other projects/concurrent computing/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <chrono>
 
-const int kNumThreads = 1;
+int kNumThreads = 1;
 int ans = 0;
 
 int is_prime(int x) {
@@ -19,29 +20,37 @@ int is_prime(int x) {
 
 std::mutex exclusive;
 
-void check(int n, int id) {
+void check(int l, int r, int id) {
+  printf("%d: %d %d\n", id, l, r);
   int sum = 0;
-  for (int i = id; i <= n; i += kNumThreads) {
+  for (int i = l; i <= r; i++) {
     sum += is_prime(i);
   }
   exclusive.lock();
   ans += sum;
   exclusive.unlock();
+  printf("%d: %d\n", id, sum);
 }
 
-int main() {
-  int start = clock();
+int main(int argc, char **argv) {
+  if (argc > 1) {
+    sscanf(argv[1], "%d", &kNumThreads);
+  }
+  auto start = std::chrono::high_resolution_clock::now();
   int n = 10000000;
   std::thread *threads[kNumThreads];
-  for (int i = 0; i < kNumThreads; i++) {
-    threads[i] = new std::thread(check, n, i);
+  for (int i = 0, l = 1, r; i < kNumThreads; i++, l = r + 1) {
+    r = l + n / kNumThreads + 1;
+    r = std::min(r, n);
+    threads[i] = new std::thread(check, l, r, i);
   }
   for (int i = 0; i < kNumThreads; i++) {
     threads[i]->join();
     delete threads[i];
   }
   printf("%d\n", ans);
-  int end = clock();
-  printf("%.5f\n", double(end - start) / CLOCKS_PER_SEC);
+  auto stop = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+  printf("duration = %dus\n", duration.count());
   return 0;
 }
